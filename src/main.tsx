84fbcbf60ef630c3808a89a8023eb57834a9c41f
felipel7 <felipelseven@gmail.com>
2024-01-23 17:29:01 -0300

    chore: add Roboto font

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { ThemeProvider } from "@mui/material/styles";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes.tsx";
-import theme from "./theme.tsx";
+import { ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
+import router from './routes.tsx';
+import theme from './theme.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
