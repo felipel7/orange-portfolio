e7ec2b3f1fa0de87e71d8e9f5170707b786a57d8
felipel7 <felipelseven@gmail.com>
2024-01-23 17:40:46 -0300

    feat(routes): add login route

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
     <>
-      <h1>Navbar</h1>
-      <Button>teste</Button>
+      {/* TODO: <Navbar /> */}
       <Outlet />
     </>
   );
