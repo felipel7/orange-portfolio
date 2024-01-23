263375e7061a9ec2e781c489190089f88ce0fc12
felipel7 <felipelseven@gmail.com>
2024-01-23 17:49:55 -0300

    feat(LoginPage): add page initial structure

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,20 +1,36 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, darken } from '@mui/material/styles';
 
 const theme = createTheme({
-  palette: {
-    // suas outras configurações de cores
-  },
+  palette: {},
   components: {
     MuiButton: {
       styleOverrides: {
         root: {
-          backgroundColor: "rgb(255, 136, 51)",
-          color: "white",
-          "&:hover": {
-            backgroundColor: "rgb(229, 126, 34)",
+          background: '#FF5522',
+          color: '#FCFDFF',
+          fontWeight: 'bold',
+          '&:hover': {
+            backgroundColor: darken('#FF5522', 0.05),
           },
+          letterSpacing: 0.5,
         },
       },
+    },
+  },
+  typography: {
+    h3: {
+      fontSize: 48,
+      '@media (max-width:600px)': {
+        fontSize: 24,
+      },
+      color: '#222244',
+    },
+    h5: {
+      fontSize: 24,
+      '@media (max-width:600px)': {
+        fontSize: 16,
+      },
+      color: '#515255',
     },
   },
 });
