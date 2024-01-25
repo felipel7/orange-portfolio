8b4053920986b1ff9bd08b588e2ce4b6bf76a333
Marcos Salles <macosrs805@gmail.com>
2024-01-25 15:17:17 -0300

    Add page and inputs for register

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,12 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Layout from './Layout';
-import HomePage from './pages/Home';
-import LoginPage from './pages/LoginPage';
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },
@@ -13,8 +14,12 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: '/login',
+    path: "/login",
     element: <LoginPage />,
+  },
+  {
+    path: "/registrar",
+    element: <RegisterPage />,
   },
 ]);
 
