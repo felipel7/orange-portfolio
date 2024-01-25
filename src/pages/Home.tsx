72fb9f046a58de33613b6eedc50fdf9e0be214c2
Marcos Salles <macosrs805@gmail.com>
2024-01-25 15:38:39 -0300

    Add Modal suce message

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { PhotoLibrary } from '@mui/icons-material';
+import { PhotoLibrary } from "@mui/icons-material";
 import {
   Autocomplete,
   Box,
@@ -7,27 +7,33 @@ import {
   Stack,
   TextField,
   Typography,
-} from '@mui/material';
-import UserActionPanel from '../components/UserActionPanel';
+} from "@mui/material";
+import UserActionPanel from "../components/UserActionPanel";
+import SuccessDialog from "../components/ModalSucess/SuccessDialog";
 
 export default function HomePage() {
   return (
     <Container maxWidth="xl">
+      <SuccessDialog
+        onClose={() => console.log("")}
+        open={true}
+        text="teste de modal"
+      />
       <Stack px={1}>
-        <Box mt={{ xs: 7, sm: '112px' }} mb={{ xs: 5, sm: 7 }}>
+        <Box mt={{ xs: 7, sm: "112px" }} mb={{ xs: 5, sm: 7 }}>
           <UserActionPanel />
         </Box>
         <AppSearch />
 
         <ImageList
           sx={{
-            gridAutoFlow: 'column',
+            gridAutoFlow: "column",
             gridTemplateColumns: {
-              xs: 'repeat(auto-fill, minmax(300px, 1fr)) !important',
-              sm: 'repeat(auto-fill, 389px) !important',
+              xs: "repeat(auto-fill, minmax(300px, 1fr)) !important",
+              sm: "repeat(auto-fill, 389px) !important",
             },
-            columnGap: '24px !important',
-            height: '258px',
+            columnGap: "24px !important",
+            height: "258px",
           }}
         >
           {/* TODO: integrar os cards com o backend... 
@@ -47,7 +53,7 @@ export default function HomePage() {
             sx={{ height: 258 }}
             px={{ xs: 4, sm: 8 }}
           >
-            <PhotoLibrary fontSize="large" sx={{ fill: '#323232' }} />
+            <PhotoLibrary fontSize="large" sx={{ fill: "#323232" }} />
             <Typography>Adicione seu primeiro projeto</Typography>
             <Typography fontSize={14}>
               Compartilhe seu talento com milhares de pessoas
@@ -67,7 +73,7 @@ function AppSearch() {
         id="free-solo-demo"
         freeSolo
         options={[]}
-        renderInput={params => <TextField {...params} label="Buscar tags" />}
+        renderInput={(params) => <TextField {...params} label="Buscar tags" />}
         sx={{ maxWidth: 512, marginTop: 2, marginBottom: { xs: 3, sm: 5 } }}
       />
     </Typography>
