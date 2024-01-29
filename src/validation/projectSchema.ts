6e0bc38fd31f721bacccf759e3b0b8ad1f774042
felipel7 <felipelseven@gmail.com>
2024-01-29 09:39:31 -0300

    feat(ProjectForm): add image upload via cloudinary

--- src/validation/projectSchema.ts
+++ src/validation/projectSchema.ts
@@ -9,5 +9,9 @@ export const projectSchema = z.object({
     .max(255, 'Deve ter no máximo 255 letras'),
   tags: z.array(z.string()),
   link: z.string().min(1, 'Não pode estar vazio'),
-  description: z.string().max(2000, 'Deve ter no máximo 2000 caracteres'),
+  description: z
+    .string()
+    .min(1, 'Não pode estar vazio')
+    .max(2000, 'Deve ter no máximo 2000 caracteres'),
+  imageProject: z.any(),
 });
