d2ec68ed5be32d42c8eb3ee874620734c7c1cd00
felipel7 <felipelseven@gmail.com>
2024-01-30 14:39:39 -0300

    chore: update home and discovery page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Box, Container, Stack } from '@mui/material';
 import { useState } from 'react';
 import UserCard from '../components/Cards/UserCard';
 import ProjectModalForm from '../components/Forms/ProjectModalForm';
-import ProjectList from '../components/ProjectList';
+import SearchableProjects from '../components/SearchableProjects';
 
 export default function HomePage() {
   const [openFormModal, setOpenFormModal] = useState(false);
@@ -14,7 +14,11 @@ export default function HomePage() {
           <Box mt={{ xs: 7, sm: '112px' }} mb={{ xs: 5, sm: 7 }}>
             <UserCard />
           </Box>
-          <ProjectList setOpenFormModal={setOpenFormModal} />
+          <SearchableProjects
+            action={() => setOpenFormModal(true)}
+            searchBarLabel="Meus Projetos"
+            showTags
+          />
         </Stack>
       </Container>
       <ProjectModalForm open={openFormModal} setOpen={setOpenFormModal} />
