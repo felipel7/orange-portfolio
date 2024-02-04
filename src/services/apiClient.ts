b35e6c154b466596010342ecf83c23a545e12657
felipel7 <felipelseven@gmail.com>
2024-02-04 09:04:43 -0300

    fix: get token from localStorage

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -4,11 +4,11 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use(async config => {
   const token = localStorage.getItem('token');
 
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${await JSON.parse(token)}`;
   }
 
   return config;
